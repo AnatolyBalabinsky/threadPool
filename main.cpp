fc37ca642988c14025ae05e2a8a30500f4aa2475
AnatolyBalabinsky <anatoly.balabinsky@gmail.com>
2023-11-24 16:16:22 +0300

    add main.cpp, no thread

--- main.cpp
+++ main.cpp
@@ -1,20 +1,61 @@
 #include "printer/printer.h"
+#include <filesystem>
+#include "reader/reader.h"
+#include "writer/writer.h"
+#include "task/taskMult.h"
+#include "task/taskSum.h"
+#include "task/taskSumSqr.h"
 
-int main() {
-    Printing::Printer printer;
+int main( int argc, char* argv[] ) {
 
-    printer.print( " Hello Word! " );
-    std::cerr << " принтеров создано: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
+    std::filesystem::path directory( argv[ 1 ] );
 
-    Printing::Printer* ptrToprinter = nullptr;
+    if( !std::filesystem::exists( directory ) ) {
+        std::cout << "Директория не существует" << std::endl;
+        return 0;
+    }
 
-    ptrToprinter = new Printing::Printer();
+    std::filesystem::directory_iterator begin( directory );
+    std::filesystem::directory_iterator end;
 
-    std::cerr << " принтеров создано: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
+    float result = 0;
+    for( ; begin != end; ++begin ) {
 
-    delete ptrToprinter;
+        if( begin->path().filename() == "out.dat" ) {
+            continue;
+        }
 
-    std::cerr << " принтеров создано: " << Printing::Printer::getCountCreatedPrinters() << std::endl;
+        // Вывод имени файла
+        std::cout << begin->path().filename() << std::endl;
+
+        Reader reader( begin->path() );
+        Reader::FileData fd = reader.getFileData();
+
+        TaskFactory factory;
+        TaskFactory::TaskType type;
+        if( fd.operType == 1 ) {
+            type = TaskFactory::TaskType::sum;
+        }
+        if( fd.operType == 2 ) {
+            type = TaskFactory::TaskType::mult;
+        }
+        if( fd.operType == 3 ) {
+            type = TaskFactory::TaskType::sumSqr;
+        }
+
+        auto task = factory.createTask( type );
+        std::cerr << "data size: " << fd.fileData.size() << std::endl;
+        auto currRes = task->process( fd.fileData );
+        std::cerr << "currRes: " << currRes << std::endl;
+        result += currRes;
+        std::cerr << "result: " << result << std::endl;
+
+    }
+
+    std::cout << std::fixed << std::setprecision( 4 ) << result << std::endl;
+
+    Writer writer( "myOut.dat" );
+    writer.write( result );
 
 	return 0;
 }
