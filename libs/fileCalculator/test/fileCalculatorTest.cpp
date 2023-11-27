8ebb0be2596306f74eaead03743edf853e9c62d2
AnatolyBalabinsky <anatoly.balabinsky@gmail.com>
2023-11-27 18:58:56 +0300

    add test for checking time of threads

--- libs/fileCalculator/test/fileCalculatorTest.cpp
+++ libs/fileCalculator/test/fileCalculatorTest.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <ctime>
+
 #include "reader/reader.h"
 #include "fileCalculator/fileCalculator.h"
 
@@ -30,3 +32,21 @@ TEST( FileCalculatorTest, ResultComparing ) {
 
 }
 
+TEST( FileCalculatorTest, TimeComparing ) {
+
+    FileCalculator calculator;
+
+    std::string dirPath( "sourceDat/testData_100" );
+
+    double startTime = clock();
+    calculator.oneThread( dirPath );
+    double time1 = clock() - startTime;
+
+    double startTime2 = clock();
+    calculator.multipleThread( dirPath );
+    double time2 = clock() - startTime2;
+
+    std::cout << "time of oneThread: " << time1 << std::endl;
+    std::cout << "time of multipleThread: " << time2 << std::endl;
+
+}
