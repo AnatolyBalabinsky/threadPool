6d8ed95b2c88f9e5bd878b4dc4182cfd5b18d0e3
AnatolyBalabinsky <anatoly.balabinsky@gmail.com>
2023-11-27 20:05:51 +0300

    fix test of time

--- libs/fileCalculator/test/fileCalculatorTest.cpp
+++ libs/fileCalculator/test/fileCalculatorTest.cpp
@@ -8,7 +8,7 @@ TEST( FileCalculatorTest, oneThread ) {
 
     FileCalculator calculator;
     auto result = calculator.oneThread( "sourceDat/testData_100" );
-    ASSERT_NEAR( result, 1355.785, 1e-6 );
+    ASSERT_NEAR( result, 1355.785, 1e-3 );
 }
 
 TEST( FileCalculatorTest, multipleThread ) {
@@ -38,15 +38,18 @@ TEST( FileCalculatorTest, TimeComparing ) {
 
     std::string dirPath( "sourceDat/testData_100" );
 
-    double startTime = clock();
+    auto t1 = std::chrono::system_clock::now();
     calculator.oneThread( dirPath );
-    double time1 = clock() - startTime;
+    auto t2 = std::chrono::system_clock::now();
 
-    double startTime2 = clock();
+    auto t3 = std::chrono::system_clock::now();
     calculator.multipleThread( dirPath );
-    double time2 = clock() - startTime2;
+    auto t4 = std::chrono::system_clock::now();
 
-    std::cout << "time of oneThread: " << time1 << std::endl;
-    std::cout << "time of multipleThread: " << time2 << std::endl;
+    std::chrono::duration< double > time1 = t2 - t1;
+    std::chrono::duration< double > time2 = t4 - t3;
+
+    std::cout << "time of oneThread: " << time1.count() << std::endl;
+    std::cout << "time of multipleThread: " << time2.count() << std::endl;
 
 }
