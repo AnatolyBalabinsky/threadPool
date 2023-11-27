da87c4652b0adc3a956c18b7dc566ad7dba995de
AnatolyBalabinsky <anatoly.balabinsky@gmail.com>
2023-11-27 18:34:02 +0300

    add files for tests

--- libs/fileCalculator/test/fileCalculatorTest.cpp
+++ libs/fileCalculator/test/fileCalculatorTest.cpp
@@ -6,14 +6,14 @@
 TEST( FileCalculatorTest, oneThread ) {
 
     FileCalculator calculator;
-    auto result = calculator.oneThread( "/home/anatoly/work/workC++/testFiles/debug-dpdk/testData_100" );
+    auto result = calculator.oneThread( "sourceDat/testData_100" );
     ASSERT_NEAR( result, 1355.785, 1e-6 );
 }
 
 TEST( FileCalculatorTest, multipleThread ) {
 
     FileCalculator calculator;
-    auto result = calculator.multipleThread( "/home/anatoly/work/workC++/testFiles/debug-dpdk/testData_100" );
+    auto result = calculator.multipleThread( "sourceDat/testData_100" );
     ASSERT_NEAR( result, 1355.785, 1e-3 );
 
 }
@@ -22,7 +22,7 @@ TEST( FileCalculatorTest, ResultComparing ) {
 
     FileCalculator calculator;
 
-    std::string dirPath( "/home/anatoly/work/workC++/testFiles/debug-dpdk/testData_100" );
+    std::string dirPath( "sourceDat/testData_100" );
 
     auto resMultiple = calculator.multipleThread( dirPath );
     auto resOne = calculator.oneThread( dirPath );
